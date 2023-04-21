fbf5fcb586995d484c4964773ca5a6dd2546e903
kiordev <kiordev@gmail.com>
2023-04-21 15:52:33 +0300

    - Добавлен функционал для NoteBook Frame

--- root.py
+++ root.py
@@ -25,19 +25,6 @@ root.resizable(False, False)
 root.title("Mentala")
 
 # Functions
-def save_content():
-    content = my_text.get("1.0", "end-1c")
-    with open("saved_content.txt", "w") as f:
-        f.write(content)
-
-# App Menu
-main_menu = tkb.Menu(root)
-root.config(menu=main_menu)
-
-# Menu Items
-file_menu = tkb.Menu(main_menu, tearoff=False)
-main_menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_cascade(label="Save", command=save_content)
 
 # Root_Grid_Configure
 root.rowconfigure(0, weight=1)
@@ -64,29 +51,50 @@ version_label = tkb.Label(menu_frame, text="Mentala Beta 2023 | 0.3", font=("Got
 version_label.pack(anchor="sw", side=tkb.BOTTOM, padx=10)
 # ------Menu_Frame------
 
-# ==========Create NoteBook_Widget=========
+# ==========Create NoteBook_Frame=========
 notebook_main_frame = tkb.Frame(root, bootstyle="dark")
 notebook_main_frame.grid(row=0, column=1, sticky="nsew")
-# Test_Frame_Name
-test_name_label = tkb.Label(notebook_main_frame, text="НОТАТКИ", font=("Gotham-bold", 15), bootstyle="inverse-dark")
-test_name_label.pack(pady=10)
+
+# NoteBook_Frame_Name
+notebook_name_label = tkb.Label(notebook_main_frame, text="НОТАТКИ", font=("Gotham-bold", 15), bootstyle="inverse-dark")
+notebook_name_label.pack(pady=10)
+
+# NoteBook_Widget
 my_notebook = tkb.Notebook(notebook_main_frame, bootstyle='dark')
 my_notebook.pack(pady=20)
+
 # Create PAGE
 tab1 = tkb.Frame(my_notebook)
-# Create TextField
-my_text = tkb.Text(tab1, width=70, height=10)
-my_text.pack(pady=10, padx=10)
-# Add Frame
 my_notebook.add(tab1, text='NOTES')
+
+# Create a Text Field
+my_text = tkb.Text(tab1, height=10, width=90)
+my_text.pack()
+def save_notebook_content():
+    file = open('notes.txt', 'w')
+    content = my_text.get("1.0", "end-1c")
+    file.write(content)
+try:
+    file = open('notes.txt', 'r')
+    content = file.read()
+    my_text.insert('end', content)
+except FileNotFoundError:
+    pass
+
+# Save NoteBook Text Button
+save_button = tkb.Button(notebook_main_frame, text="СОХРАНИТЬ", bootstyle='primary', command=save_notebook_content)
+save_button.pack(pady=10)
+
 # ==========Create NoteBook_Widget=========
 
 # ==========Tests_Frame=========
+
 tests_main_frame = tkb.Frame(root, bootstyle="dark")
 tests_main_frame.grid(row=0, column=1, sticky="nsew")
 # Test_Frame_Name
 test_name_label = tkb.Label(tests_main_frame, text="ТЕСТУВАННЯ", font=("Gotham-bold", 15), bootstyle="inverse-dark")
 test_name_label.pack(pady=10)
+
 # ==========Tests_Frame=========
 
 # ==========Table_Frame=========
