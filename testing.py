569caa8b73ef4d0563cf8c5a425b392d3df60fc0
kiordev <kiordev@gmail.com>
2023-04-26 16:36:39 +0300

    - minor changes

--- testing.py
+++
@@ -1,9 +0,0 @@
-import ttkbootstrap as tkb
-from ttkbootstrap.constants import *
-from ttkbootstrap.scrolled import ScrolledFrame
-# Settings
-main_window = tkb.Window(themename="vapor")
-main_window.geometry("1000x500")
-main_window.resizable(False, False)
-
-main_window.mainloop()
